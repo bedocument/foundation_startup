d107ae648e25571bb2f70c46a688c32dd889ec69
bedocument <babe7786@gmail.com>
2016-02-02 14:01:08 +0800

    RWD Chapter 7/Chapter 8 Example test OK

---
+++ startup/assets/js/foundation.js
@@ -0,0 +1,4 @@
+// @koala-prepend "../../bower_components/foundation/js/vendor/jquery.js"
+// @koala-prepend "../../bower_components/foundation/js/foundation/foundation.js"
+// @koala-prepend "../../bower_components/foundation/js/foundation/foundation.topbar.js"
+// @koala-prepend "../../bower_components/foundation/js/foundation/foundation.orbit.js"
